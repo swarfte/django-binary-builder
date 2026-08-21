264a983daea04e0542cf1bde6b4268ce734298db
Benjamin Chau <68836494+swarfte@users.noreply.github.com>
2026-08-21 10:23:23 +0800

    setup project structure

---
+++ src/django_binary_builder/app.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class DjangoBinaryBuilderConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "django_binary_builder"
+    verbose_name = "Django Binary Builder"
